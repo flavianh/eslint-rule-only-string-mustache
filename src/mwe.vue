e258e202f9d593ab654a899e7641c9708f0d609a
flavianh <flavian@apricity.life>
2022-07-18 18:17:14 +0000

    Add capture the flag objectives

--- src/mwe.vue
+++ src/mwe.vue
@@ -1,18 +1,26 @@
-<template>
-  {{ totalNumber }}
-</template>
-
 <script>
+import Vue from "vue";
 
-export default {
+export default new Vue({
+  data: {},
   computed: {
+    flag: function () {
+      return
+    },
+
     /**
      * @returns {number}
-     */ 
+     */
     totalNumber: function () {
-        // TODO: Should show an error
-        return 'rty';
-    }
+      // TODO: Should show an error
+      return 2149;
+    },
   }
-}
+});
 </script>
+<template>
+  <!-- Objective 1: capture `flag` with eslint and vuejs parser -->
+  <span>{{ flag }}</span>
+  <!-- Objective 2: capture the return type of the `totalNumber` computed property -->
+  <span>Il reste {{ totalNumber }} places.</span>
+</template>
