9f613e45e93b647b9ba7f2c8957a27e69de6e124
flavianh <flavian@apricity.life>
2022-07-18 17:52:58 +0000

    Configure (broken) minimal working example

---
+++ src/mwe.vue
@@ -0,0 +1,18 @@
+<template>
+  {{ totalNumber }}
+</template>
+
+<script>
+
+export default {
+  computed: {
+    /**
+     * @returns {number}
+     */ 
+    totalNumber: function () {
+        // TODO: Should fail
+        return 'rty';
+    }
+  }
+}
+</script>
