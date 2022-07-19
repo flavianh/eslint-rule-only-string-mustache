0e0def9d297ac886f2177d627d1c95245c4b97cb
flavianh <flavian@apricity.life>
2022-07-19 06:55:10 +0000

    Clarify challenges

--- src/mwe.vue
+++ src/mwe.vue
@@ -1,26 +1,29 @@
 <script>
+
+// Challenge: write ESlint rules to achieve objective 1 then objective 2.
+
 import Vue from "vue";
 
 export default new Vue({
   data: {},
   computed: {
     flag: function () {
-      return
+      return;
     },
 
     /**
      * @returns {number}
      */
+    // Objective 2: for all mustaches in the template which display a computed property, capture the return type and output an error below if number
+    // totalNumber should be underlined in red by eslint.
     totalNumber: function () {
-      // TODO: Should show an error
       return 2149;
     },
   }
 });
 </script>
 <template>
-  <!-- Objective 1: capture `flag` with eslint and vuejs parser -->
+  <!-- Objective 1: capture the term `flag` with eslint and vuejs parser below. Flag should be underlined in red by eslint. -->
   <span>{{ flag }}</span>
-  <!-- Objective 2: capture the return type of the `totalNumber` computed property -->
   <span>Il reste {{ totalNumber }} places.</span>
 </template>
