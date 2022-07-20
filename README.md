953a4a8bec13a7d5f153697d860a19a4bab557b0
flavianh <flavian@apricity.life>
2022-07-20 10:28:35 +0000

    Add README

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-Test
+The target is to solve the objectives.
+
+- Everything is in the file src/mwe.vue
+- JSDoc type inference with Typescript is activated
+- What's left is to add the eslint custom rules to capture the three flags
+
+As help, a blueprint for objective 3 is the following:
+![](./eslint-rule-mustache-string.png)
--- src/mwe.vue
+++ src/mwe.vue
@@ -16,6 +16,9 @@ export default new Vue({
      */
     // Objective 3: for all mustaches in the template which display a computed property, capture the return type and output an error below if number
     // totalNumber should be underlined in red by eslint.
+    // Useful resources:
+    // - https://github.com/phenomnomnominal/tsquery
+    // - https://urish.medium.com/diving-into-the-internals-of-typescript-how-i-built-typewiz-d273bbef3565
     totalNumber: function () {
       return 2149;
     },
@@ -26,6 +29,12 @@ export default new Vue({
   <!-- Objective 1: Install eslint and trigger the rule [vue mustache interpolation spacing](https://eslint.vuejs.org/rules/mustache-interpolation-spacing.html#vue-mustache-interpolation-spacing) -->
   <span>{{flag}}</span>
   <!-- Objective 2: capture the term `flag` with eslint and vuejs parser below. Flag should be underlined in red by eslint. -->
+  <!-- Useful resources:
+    - https://github.com/vuejs/eslint-plugin-vue/tree/master/lib/rules to get inspiration around eslint rules for vuejs
+    - https://github.com/vuejs/eslint-plugin-vue/issues/262 
+    - https://eslint.org/docs/latest/developer-guide/working-with-rules
+    - https://github.com/vuejs/vue-eslint-parser
+    -->
   <span>{{ flag }}</span>
   <span>Remains {{ totalNumber }} places.</span>
 </template>
